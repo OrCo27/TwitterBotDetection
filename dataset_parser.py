c460cfaf8a3041ad5eee6d0dce63b2ba7a163d37
Or Cohen <or.cohen22@gmail.com>
2020-04-20 10:19:58 +0300

    fix generating dataset pairs by users

--- dataset_parser.py
+++ dataset_parser.py
@@ -1,14 +1,13 @@
 from utils_nnet import ModelCommon as utils
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import random
 
 
 # enum for possible dataset configuration
 class DatasetConfig:
-    RANDOM_STATE = [0, 'random_resolution']  # select random pairs from the whole dataset
-    USER_STATE = [1, 'user_resolution']      # select pairs from each user
+    RANDOM_STATE = [0, 'random']  # select random pairs from the whole dataset
+    USER_STATE = [1, 'user']      # select pairs from each user
 
 
 class DatasetBuilder:
@@ -33,13 +32,10 @@ class DatasetBuilder:
         # build final triples by users resolution
         if self.config_id == DatasetConfig.USER_STATE[0]:
             users_dict = self._build_users_dict(query_list)
-            final_bots, final_docs, final_labels = self._generate_final_dataset_user_groups(users_dict,
-                                                                                         query_list,
-                                                                                         doc_list)
+            final_bots, final_docs, final_labels = self._generate_dataset_users(users_dict, query_list, doc_list)
         # build final triples by random state
         else:
-            final_bots, final_docs, final_labels = self._generate_final_dataset_random(query_list,
-                                                                                        doc_list)
+            final_bots, final_docs, final_labels = self._generate_dataset_random(query_list, doc_list)
 
         # build the vocabulary based on query-doc lists
         self.max_text_len = self._build_vocab_docs(final_bots, final_docs)
@@ -47,12 +43,8 @@ class DatasetBuilder:
         # generate additional feature list only if needed
         self._generate_additional_feats(final_bots, final_docs, additional_feats_enabled)
 
-        # convert texts to sequences
-        x_query_list = self._convert_text_to_sequences(final_bots, self.max_text_len)
-        x_doc_list = self._convert_text_to_sequences(final_docs, self.max_text_len)
-
-        self.query_list = x_query_list
-        self.doc_list = x_doc_list
+        self.query_list = final_bots
+        self.doc_list = final_docs
         self.labels_list = np.array(final_labels).astype('int32')
 
         self.logger.write_log(f'Datasets completed with ({len(query_list)}) lines')
@@ -78,10 +70,6 @@ class DatasetBuilder:
 
         return max_len
 
-    def _convert_text_to_sequences(self, text_list, max_text_len):
-        x_text = self.tokenizer.texts_to_sequences(text_list)
-        return pad_sequences(x_text, maxlen=max_text_len, padding='post', truncating='post')
-
     # query_file is list of queries before pre-processing
     def _build_users_dict(self, query_file):
         self.logger.write_log(f'Building dictionary of each user and its tweets')
@@ -97,8 +85,10 @@ class DatasetBuilder:
             if curr_user is not None:
                 if curr_user not in users_dict:  # it's the first tweet of this user, add it
                     users_dict[curr_user] = [line]
-                elif line not in users_dict[curr_user]:  # append to exists user only if not exists
+                else:
                     users_dict[curr_user].append(line)
+                # elif line not in users_dict[curr_user]:  # append to exists user only if not exists
+                #     users_dict[curr_user].append(line)
 
         return users_dict
 
@@ -113,7 +103,7 @@ class DatasetBuilder:
         return bot_tweet, doc_tweet, length_valid
 
     # build queries and docs pairs by user resolution
-    def _generate_final_dataset_user_groups(self, users_dict, bot_lines, human_lines):
+    def _generate_dataset_users(self, users_dict, bot_lines, human_lines):
         self.logger.write_log(f'Generating final dataset with appropriate candidates...')
 
         final_bots = []
@@ -124,40 +114,38 @@ class DatasetBuilder:
             items_count = len(users_dict[user])
             if items_count == 1:  # current user have only one tweet, pair to random human tweet
                 bot_tweet = users_dict[user][0]
-                human_choice = bool(random.getrandbits(1))  # random choice between bot or human source
-
-                if human_choice:
-                    doc_tweet = random.choice(human_lines)
-                    label = 0
-                else:
-                    doc_tweet = random.choice(bot_lines)
-                    label = 1
+                doc_tweet = random.choice(human_lines)
 
                 bot_tweet, doc_tweet, length_valid = self._perform_pre_processing(bot_tweet, doc_tweet)
 
                 if length_valid:
                     final_bots.append(bot_tweet)
                     final_docs.append(doc_tweet)
-                    final_labels.append(label)
+                    final_labels.append(0)
             else:  # user have more than one tweet, iterate them and random choose from bot or human source
-                list_items = list(range(1, items_count))
+                list_items = list(range(0, items_count))
                 for i in range(items_count):
                     curr_tweet = users_dict[user][i]
-                    human_choice = bool(random.getrandbits(1))  # random choice between bot or human source
 
-                    if human_choice or len(list_items) == 0:  # select from human source
+                    if i in list_items:  # remove current index, so it will not select again
+                        list_items.remove(i)
+
+                    #human_choice = bool(random.getrandbits(1))  # random choice between bot or human source
+
+                    if len(list_items) == 0:  # select from human source
                         doc_tweet = random.choice(human_lines)
                         label = 0
                     else:  # select from bot user list
                         candidate_random_index = random.choice(list_items)
                         doc_tweet = users_dict[user][candidate_random_index]
-                        label = 1
+                        list_items.remove(candidate_random_index)  # remove selected index that it will not select again
+
                         # if pairs are not equals, select from own list
-                        # and remove selected index that it will not select again
                         if doc_tweet != curr_tweet:
-                            list_items.remove(candidate_random_index)
-                        else:  # pairs are equals, choose from others bots tweets
-                            doc_tweet = random.choice(bot_lines)
+                            label = 1
+                        else:  # pairs are equals, choose from humans tweets
+                            doc_tweet = random.choice(human_lines)
+                            label = 0
 
                     curr_tweet, doc_tweet, length_valid = self._perform_pre_processing(curr_tweet, doc_tweet)
                     if length_valid:
@@ -180,7 +168,7 @@ class DatasetBuilder:
             self.addit_feat_len = self.overlap_feats.shape[1]
 
     # generate docs pairs from the whole bots source
-    def _generate_final_dataset_random(self, query_list, doc_list):
+    def _generate_dataset_random(self, query_list, doc_list):
         self.logger.write_log(f'Generating final dataset with appropriate candidates...')
 
         # create temporary labels for each list
