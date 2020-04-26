e7c3603c1aabc67c98adb1dffe5bb1ac97b3ad96
Or Cohen <or.cohen22@gmail.com>
2020-04-26 14:49:38 +0300

    add multiple tweets predict feature

--- run_nnet.py
+++ run_nnet.py
@@ -3,40 +3,45 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import os
 import pickle
+import csv
+import random
 
-
-class ModelPredicter:
+class SinglePredictor:
     def __init__(self, model_name):
         self.model_name = model_name
-
-    def predict(self, tweet_pred):
         # load model parameters
-        model, x_bot_list, bot_list, tokenizer, max_text_len = self._load_model()
+        self.model = None
+        self.tokenizer = None
+        self.max_text_len = None
+        self.x_bot_list = []
+        self.bot_list = []
 
+    # predict bot similarity score on a single tweet
+    def predict(self, tweet_pred):
         # perform pre-processing
         clean_tweet_pred = utils.preprocess_tweet(tweet_pred)
 
         # build doc list by duplicate tweet prediction foreach line in bot list
-        tweet_pred_list = [clean_tweet_pred] * len(bot_list)
+        tweet_pred_list = [clean_tweet_pred] * len(self.bot_list)
 
         # convert tweet predicted to sequence
         temp_pred_list = [clean_tweet_pred]
-        x_temp_pred_list = utils.convert_text_to_sequences(tokenizer, temp_pred_list, max_text_len)
+        x_temp_pred_list = utils.convert_text_to_sequences(self.tokenizer, temp_pred_list, self.max_text_len)
 
         # duplicate sequence to the length of bot size list
-        x_doc_list = [x_temp_pred_list[0]] * len(bot_list)
+        x_doc_list = [x_temp_pred_list[0]] * len(self.bot_list)
         x_doc_list = np.array(x_doc_list)
 
         # calculate word overlapping additional feature
-        additional_feat = utils.compute_overlap_features(bot_list, tweet_pred_list)
+        additional_feat = utils.compute_overlap_features(self.bot_list, tweet_pred_list)
 
-        predict_list = model.predict([x_bot_list, x_doc_list, additional_feat], verbose=1)
+        predict_list = self.model.predict([self.x_bot_list, x_doc_list, additional_feat], verbose=1)
 
         bot_similarity_score = len(list(filter(lambda x: x > 0.5, predict_list)))/len(predict_list)
 
         return bot_similarity_score
 
-    def _load_model(self):
+    def load_model(self):
         full_path = os.path.join('output', self.model_name)
         model_path = full_path + '.h5'
         pickle_path = full_path + '.pickle'
@@ -45,4 +50,57 @@ class ModelPredicter:
         with open(pickle_path, 'rb') as f:
             x_bot_list, bot_list, tokenizer, max_text_len = pickle.load(f)
 
-        return model, x_bot_list, bot_list, tokenizer, max_text_len
+        self.model = model
+        self.x_bot_list = x_bot_list
+        self.bot_list = bot_list
+        self.tokenizer = tokenizer
+        self.max_text_len = max_text_len
+
+
+class MultiPredictor(SinglePredictor):
+    def __init__(self, model_name):
+        super().__init__(model_name)
+        self.tweets_text_list = [] # original tweets text
+        self.tweets_preds_list = [] # predicts score for each tweet
+        self.tweets_bot_class = [] # 1 for bot, 0 for human
+
+    # loads txt and csv file
+    # NOTE: csv file must include only one column with tweet content
+    def load_file_content(self, tweet_file, ignore_header=False):
+        tweets_list = []
+
+        if tweet_file.endswith('.txt'):
+            with open(tweet_file, 'r', encoding='utf-8') as f:
+                tweets_list = f.readlines()
+
+        elif tweet_file.endswith('.csv'):
+            with open(tweet_file, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    tweets_list.append(row[0])
+
+        if ignore_header:
+            tweets_list.remove(tweets_list[0])
+
+        self.tweets_text_list = tweets_list
+
+    # predict multiple tweets from specific file
+    def predict(self, take_random_tweets=150):
+        if len(self.tweets_text_list) > take_random_tweets:
+            self.tweets_text_list = random.sample(self.tweets_text_list, take_random_tweets)
+
+        for tweet in self.tweets_text_list:
+            sim_bot_score = super().predict(tweet)
+            self.tweets_preds_list.append(sim_bot_score)
+
+    def _check_if_bot(self, score, threshold):
+        return 1 if (score >= threshold) else 0
+
+    # create the classification of bots based on threshold
+    def classify_by_threshold(self, threshold=0.5):
+        self.tweets_bot_class = [self._check_if_bot(x, threshold) for x in self.tweets_preds_list]
+
+    # get the bots distribution in decimal
+    def get_bots_distribution(self):
+        bot_distribution = len(list(filter(lambda x: x == 1, self.tweets_bot_class))) / len(self.tweets_bot_class)
+        return bot_distribution
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from build_nnet import ModelTrainer
 from logger import Log
 from utils_nnet import ModelCommon
 from dataset_parser import DatasetConfig
-from run_nnet import ModelPredicter
+from run_nnet import SinglePredictor, MultiPredictor
 
 # training part
 # model_train = ModelTrainer(embedding_file='data/glove.twitter.27B.200d.txt', epochs=5,
@@ -15,13 +15,23 @@ bot_tweet2 = 'Think holiday shopping with the December 2 Remember Giveaway Hop! 
 bot_tweet3 = 'Enter to win a $50 etsy giftcard! Perfect for your holiday handmade shoping! #giveaway thanx @gardendreamsdcr https://t.co/mp8H6c9KVX'
 human_tweet = 'RT @__ 489_: Do not allow my soul to extinguish Lord, I am used to my glow.'
 
-pred = ModelPredicter('model_user')
-input = bot_tweet3
-bot_score = pred.predict(input)
+# single tweet test
+# input = bot_tweet3
+# pred = SinglePredictor('model_user')
+# pred.load_model()
+# bot_score = pred.predict(input)
+#
+# print()
+# print('Tweet for predict:')
+# print(input)
+# print()
+# print(f'Similarity bot score: {bot_score:.2f}')
+# print(f'Similarity human score: {1-bot_score:.2f}')
 
-print()
-print('Tweet for predict:')
-print(input)
-print()
-print(f'Similarity bot score: {bot_score:.2f}')
-print(f'Similarity human score: {1-bot_score:.2f}')
+# muiltiple tweets test
+multi_pred = MultiPredictor('model_user')
+multi_pred.load_model()
+multi_pred.load_file_content(tweet_file='data/COVID.csv', ignore_header=True)
+multi_pred.predict(take_random_tweets=5)
+multi_pred.classify_by_threshold(threshold=0.7)
+bots_part = multi_pred.get_bots_distribution()
