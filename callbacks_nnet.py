8aa43d211f9613b6e1f7a07cd6238c930db6b8d0
Or Cohen <or.cohen22@gmail.com>
2020-05-05 18:03:20 +0300

    write accuracy and loss values every epoch to log

--- callbacks_nnet.py
+++ callbacks_nnet.py
@@ -15,19 +15,28 @@ class CallBackNNet(Callback):
         """ Called at the start of an epoch """
         self.batch_sum = 0
 
-
     def on_epoch_end(self, batch, logs={}):
         """ Called at the end of an epoch """
+        loss = logs['loss']
+        acc = logs['accuracy']
+        val_loss = logs['val_loss']
+        val_acc = logs['val_accuracy']
         epoches = self.params['epochs']
         self.epoch_cnt += 1
         epoch_progress = (self.epoch_cnt / epoches) * 100
+
+        # update progressbar and logs
         self.conf_controller.ui.progressbar_epoches.setValue(epoch_progress)
+        self.conf_controller.log.write_log(f'Epoch {self.epoch_cnt}/{epoches} | loss: {loss} - accuracy: {acc} - '
+                                           f'val_loss: {val_loss} - val_accuracy: {val_acc}')
 
     def on_batch_end(self, batch, logs=None):
         """ Called at the end of a training batch in fit methods """
         samples = self.params['samples']
         self.batch_sum += logs['size']
         batch_progress = (self.batch_sum / samples) * 100
+
+        # update progressbar
         self.conf_controller.ui.progressbar_batch.setValue(batch_progress)
 
     def on_batch_begin(self, batch, logs=None):
