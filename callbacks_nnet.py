7b4626e86c15443e079e11ec7518fab0e41aacad
Or Cohen <or.cohen22@gmail.com>
2020-05-05 17:44:25 +0300

    add progressbar processing

--- callbacks_nnet.py
+++ callbacks_nnet.py
@@ -6,20 +6,29 @@ class CallBackNNet(Callback):
         super(CallBackNNet, self).__init__()
         self.logger = logger
         self.conf_controller = conf_controller
+        self.batch_sum = 0
+        self.epoch_cnt = 0
 
     #TODO: add exit breakpoints every epoch/batch
 
     def on_epoch_begin(self, epoch, logs=None):
         """ Called at the start of an epoch """
-        pass
+        self.batch_sum = 0
+
 
     def on_epoch_end(self, batch, logs={}):
         """ Called at the end of an epoch """
-        pass
+        epoches = self.params['epochs']
+        self.epoch_cnt += 1
+        epoch_progress = (self.epoch_cnt / epoches) * 100
+        self.conf_controller.ui.progressbar_epoches.setValue(epoch_progress)
 
     def on_batch_end(self, batch, logs=None):
         """ Called at the end of a training batch in fit methods """
-        pass
+        samples = self.params['samples']
+        self.batch_sum += logs['size']
+        batch_progress = (self.batch_sum / samples) * 100
+        self.conf_controller.ui.progressbar_batch.setValue(batch_progress)
 
     def on_batch_begin(self, batch, logs=None):
         pass
